dd2b27105b91738c76066df726802324ba4d56a8
ChristopherE-crypto <christopher.esquivel@lc.cuny.edu>
2025-04-17 15:38:26 -0400

    header file for some structs

---
+++ game.h
@@ -0,0 +1,56 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <raylib.h>
+#include <vector>
+
+struct Obstacle {
+    Vector3 position;
+    Vector3 size;
+    Color color;
+    Vector3 minBounds;
+    Vector3 maxBounds;
+};
+
+struct Player {
+    Vector3 position;
+    Vector3 size; // x: width, y: height, z: length
+    Color color;
+    Vector3 minBounds;
+    Vector3 maxBounds;
+    
+    void UpdateBounds() {
+        minBounds = {
+            position.x - size.x/2,
+            position.y - size.y/2,
+            position.z - size.z/2
+        };
+        maxBounds = {
+            position.x + size.x/2,
+            position.y + size.y/2,
+            position.z + size.z/2
+        };
+    }
+};
+
+struct GameState {
+    bool running = true;
+    int score = 0;
+    float scoreTimer = 0.0f;
+    float gameOverTimer = 0.0f;
+    float currentSpeed = 5.0f;
+    
+    void Reset() {
+        running = true;
+        score = 0;
+        currentSpeed = 5.0f;
+    }
+};
+
+struct Game {
+  std::vector<Obstacle> obstaclePool;
+  int activeObstacles = 0;
+  const int MAX_OBSTACLES = 100;
+};
+
+#endif
